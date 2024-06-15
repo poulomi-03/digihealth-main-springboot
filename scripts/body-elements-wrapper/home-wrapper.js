1c998ef93b07a8a280baa3072f5c9c3b54635ef5
Suraj Gude <Suraj Gude>
2024-06-15 20:26:53 +0530

    commited many files by suraj

---
+++ scripts/body-elements-wrapper/home-wrapper.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+    let slideInterval = setInterval(nextSlide, 8000); // Change slide every 3 seconds
+  
+    function nextSlide() {
+      slides[currentSlide].style.display = 'none';
+      currentSlide = (currentSlide + 1) % slides.length;
+      slides[currentSlide].style.display = 'block';
+    }
+  });
+  
